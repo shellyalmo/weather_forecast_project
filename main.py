714bb9736bee0f8329ee7a95956ebe5bcfb7c0bc
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-09-14 10:37:18 +0300

    Update file

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from current_weather_by_city_ID_API import response
-from create_pandas_df import create_pandas_df_from_API
+from create_pandas_df import convert_json_to_python
+from update_db import weather_df, update_db
+
 # from update_db import
 # if statement for create db if no db exists
 # call function from current_weather file, and get the response
 
-(convert_json_to_python(response))
+# (convert_json_to_python(response))
 # print(create_pandas_df_from_API(response))
 # call update_db function
+
+update_db(weather_df)
