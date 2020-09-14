439d09fec57e621fda4540f7f9be8dbdc2ff446c
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-09-14 10:35:42 +0300

    Seperate create table from create db

--- create_db.py
+++ create_db.py
@@ -4,8 +4,4 @@ import pandas as pd
 connection = sqlite3.connect('TestDB.db')
 cursor = connection.cursor()
 
-# create table
-cursor.execute('''CREATE TABLE Weather
-                ([name] text, [temp] decimal(5,2), [temp_max] decimal(5,2), [temp_min] decimal(5,2), [humidity] decimal(5,2), [pressure] decimal(5,2), [wind_speed] decimal(5,2), [wind_deg] decimal(5,2))''')
 
-connection.commit()
