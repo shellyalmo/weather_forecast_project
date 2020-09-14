ca56ef92687144fb9ef392f9d06ef0bf7354671b
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-09-14 10:34:57 +0300

    first commit

---
+++ create table.sql
@@ -0,0 +1,3 @@
+-- create table
+CREATE TABLE Weather
+                ([name] TEXT, [date_time] INTEGER, [temp] DECIMAL(5,2), [temp_max] DECIMAL(5,2), [temp_min] decimal(5,2), [humidity] decimal(5,2), [pressure] decimal(5,2), [wind_speed] decimal(5,2), [wind_deg] decimal(5,2));
