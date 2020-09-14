b89cd8a163ef790cefb0fdb31616cbdcc30493a5
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-09-14 10:41:46 +0300

    Update function

--- update_db.py
+++ update_db.py
@@ -1,18 +1,19 @@
 import sqlite3
 import pandas as pd
 from current_weather_by_city_ID_API import response
-from create_db import connection, cursor
+from create_db import connection
 
 
-def update_db(pandas_df,table_name=str):
-    pandas_df.to_sql(table_name, connection, if_exists='replace', index=False)
+def update_db(pandas_df):
+    pandas_df.to_sql('Weather', connection, if_exists='replace', index=False)
 
 
-weather_data = [['Jerusalem', 'Israel', 29.0], [
-    'Madrid', 'Spain', 26.8], ['Beirut', 'Lebanon', 28.3]]
+# weather_data = [['Tel Aviv', 1599661762, 333, 334, 324, 65, 1003, 23, 325], [
+#     'Tel Aviv', 1599661762, 457, 765, 224, 68, 1034, 67, 324], ['Tel Aviv', 1599661762, 335, 338, 312, 45, 1007, 22, 329]]
 
-# Create the pandas DataFrame
-weather_df = pd.DataFrame(weather_data, columns=['City', 'Country', 'Temp'])
+# # Create the pandas DataFrame
+# weather_df = pd.DataFrame(weather_data, columns=[
+#                           'name', 'date_time', 'temp', 'temp_max', 'temp_min', 'humidity', 'pressure', 'wind_speed', 'wind_deg'])
 
 if __name__ == "__main__":
     import doctest
