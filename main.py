19bb349e2ae1c27b5cbd46ed133d984543f6378f
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-09-21 10:59:49 +0300

    Edit comments

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from update_db import update_db
 from TransformLoad import drop_col, change_col_name
 
 # if statement for create db if no db exists
-# call function from current_weather file, and get the response
 
-# (convert_json_to_python(response))
-# print(create_pandas_df_from_API(response))
-# call update_db function
 convert_dict_to_df()
 print(change_col_name(drop_col(convert_dict_to_df())))
 # update_db(weather_df)
