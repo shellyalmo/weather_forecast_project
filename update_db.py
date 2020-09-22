1c2dc00ec0d557c943e0c8c3edd1b4302702d42f
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-09-22 19:00:49 +0300

    Append instead of replace when updating the DB

--- update_db.py
+++ update_db.py
@@ -6,7 +6,7 @@ from create_db import connection
 def update_db(pandas_df):
     """ Upload pandas dataframe to sql database
     """
-    pandas_df.to_sql('Weather', connection, if_exists='replace', index=False)
+    pandas_df.to_sql('Weather', connection, if_exists='append', index=False)
 
 
 if __name__ == "__main__":
