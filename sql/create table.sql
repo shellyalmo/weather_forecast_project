ef86a1919213df8c133cb3a75bd7dd487f29dd9d
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-10-01 15:05:30 +0300

    Add to folder

---
+++ sql/create table.sql
@@ -0,0 +1,3 @@
+-- create table
+CREATE TABLE Weather
+                ([dt] PRIMARY KEY, [name] TEXT, [temp] DECIMAL(5,2), [temp_max] DECIMAL(5,2), [temp_min] DECIMAL(5,2), [humidity] DECIMAL(5,2), [pressure] DECIMAL(5,2), [wind_speed] DECIMAL(5,2), [wind_deg] DECIMAL(5,2));
