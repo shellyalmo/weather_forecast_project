4b22094f8ddab42f4158a5ff6556fb4e4561984b
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-10-04 18:30:32 +0300

    Add logs

--- get_weather_data_from_OpenWeatherMap.py
+++ get_weather_data_from_OpenWeatherMap.py
@@ -3,12 +3,14 @@ import os
 import requests
 import json
 
+
 def get_current_weather_data_from_OpenWeatherMap(id="293397"):
     """ Returns current weather data by city id (Tel Aviv by default)
     """
     load_dotenv()
     json_data = requests.get(
         "http://api.openweathermap.org/data/2.5/weather?id="+id+"&appid="+os.environ.get("api-token")).json()
+    print("Getting current weather data from OpenWeatherMap.org..........")
     return json_data
 
 
@@ -21,5 +23,3 @@ def write_weather_data_in_json_file(json_data):
     with open(filename, 'w') as f:
         json.dump(json_data, f)
     return filename
-
-
