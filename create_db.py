2b1d768b20964f07bd515968db8522dc74733d51
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-10-04 18:29:59 +0300

    Renamed function

--- create_db.py
+++ create_db.py
@@ -1,7 +1,17 @@
 import sqlite3
 
-def create_new_DB():
-    connection = sqlite3.connect('CurrentWeatherDB.db')
+
+def create_DB_connection():
+    """ Connect to a database and create cursor object for executing SQL statements.
+    """
+    connection = sqlite3.connect('CurrentWeatherDB.db', uri=True)
+    print("Connection with database established ..........")
     connection.cursor()
+    return connection
 
 
+def close_connection(connection):
+    """ Close connection with the database.
+    """
+    connection.commit()
+    connection.close()
