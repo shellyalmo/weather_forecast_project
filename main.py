b3a4b98d9f16cbfed489acb15ace445c74f364ba
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-10-04 18:33:05 +0300

    Use datetime as index rather than column

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+from create_db import create_DB_connection, close_connection
 from create_pandas_df_from_json import convert_dict_to_df
 from update_db import update_db
-from transform import drop_col, change_col_name, convert_kelvin_to_celsius
+from transform import drop_col, change_col_name, convert_kelvin_to_celsius, set_datetime_col_as_row_index
 from get_weather_data_from_OpenWeatherMap import get_current_weather_data_from_OpenWeatherMap, write_weather_data_in_json_file
 
-# if statement for create db if no db exists
-filename = write_weather_data_in_json_file(get_current_weather_data_from_OpenWeatherMap())
-new_df_to_sql = convert_kelvin_to_celsius(change_col_name(drop_col(convert_dict_to_df(filename))))
+returned_connection = create_DB_connection()
+filename = write_weather_data_in_json_file(
+    get_current_weather_data_from_OpenWeatherMap())
+new_df_to_sql = set_datetime_col_as_row_index(convert_kelvin_to_celsius(
+    change_col_name(drop_col(convert_dict_to_df(filename)))))
 
-update_db(new_df_to_sql)
+update_db(new_df_to_sql, returned_connection)
+
+close_connection(returned_connection)
