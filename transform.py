5a23f98c7d05b36e7d8f6c2862d49c8431ebbb32
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-10-07 13:59:09 +0300

    fix: match column names to original json keys

--- transform.py
+++ transform.py
@@ -1,5 +1,5 @@
 """
->>> convert_kelvin_to_celsius(df_test)
+>>> convert_kelvin_to_celsius(df_test_convert_kelvin_to_celsius)
       country   temp  temp_min  temp_max  pressure  humidity  wind_speed  wind_deg radiation
 ITA     Italy  71.85     16.85     69.85        12        56         110       180      high
 ESP     Spain  15.85     24.85     59.85        13        67          90       189       low
@@ -7,19 +7,31 @@ GRC    Greece  59.85     25.85     60.85        14        64          98       1
 FRA    France  38.85    -55.15     71.85        15        63          99       177   average
 PRT  Portugal  35.85     14.85    -41.15        11        62         100       190       low
 
->>> keep_col(df_test)
-      temp  temp_min  temp_max  pressure  humidity  wind_speed  wind_deg
-ITA  71.85     16.85     69.85        12        56         110       180
-ESP  15.85     24.85     59.85        13        67          90       189
-GRC  59.85     25.85     60.85        14        64          98       178
-FRA  38.85    -55.15     71.85        15        63          99       177
-PRT  35.85     14.85    -41.15        11        62         100       190
+>>> keep_columns(df_test_keep_columns)
+     main.temp  main.temp_min  main.temp_max  main.pressure  main.humidity  wind.speed  wind.deg
+ITA        345            290            343             12             56         110       180
+ESP        289            298            333             13             67          90       189
+GRC        333            299            334             14             64          98       178
+FRA        312            218            345             15             63          99       177
+PRT        309            288            232             11             62         100       190
 """
 
 import pandas as pd
 
 # Fake data for testing
-data_for_test = {'country': ['Italy', 'Spain', 'Greece', 'France', 'Portugal'],
+data_test_keep_columns = {'country': ['Italy', 'Spain', 'Greece', 'France', 'Portugal'],
+                         'main.temp': [345, 289, 333, 312, 309],
+                         'main.temp_min': [290, 298, 299, 218, 288],
+                         'main.temp_max': [343, 333, 334, 345, 232],
+                         'main.pressure': [12,13,14,15,11],
+                         'main.humidity': [56,67,64,63,62],
+                         'wind.speed': [110,90,98,99,100],
+                         'wind.deg': [180,189,178,177,190],
+                         'radiation':['high','low','medium','average','low']}
+
+df_test_keep_columns = pd.DataFrame(data_test_keep_columns, index=[
+                               'ITA', 'ESP', 'GRC', 'FRA', 'PRT'])
+data_test_convert_kelvin_to_celsius = {'country': ['Italy', 'Spain', 'Greece', 'France', 'Portugal'],
                          'temp': [345, 289, 333, 312, 309],
                          'temp_min': [290, 298, 299, 218, 288],
                          'temp_max': [343, 333, 334, 345, 232],
@@ -29,20 +41,19 @@ data_for_test = {'country': ['Italy', 'Spain', 'Greece', 'France', 'Portugal'],
                          'wind_deg': [180,189,178,177,190],
                          'radiation':['high','low','medium','average','low']}
 
-df_test = pd.DataFrame(data_for_test, index=[
+df_test_convert_kelvin_to_celsius = pd.DataFrame(data_test_convert_kelvin_to_celsius, index=[
                                'ITA', 'ESP', 'GRC', 'FRA', 'PRT'])
 
-
-def keep_col(pandas_df):
+def keep_columns(pandas_df):
     """ Keep only the necessary columns of the dataframe
     """
-    return pandas_df[['temp','temp_min','temp_max','pressure','humidity','wind_speed','wind_deg']]
+    return pandas_df[['main.temp','main.temp_min','main.temp_max','main.pressure','main.humidity','wind.speed','wind.deg']]
 
 
 def change_col_name(pandas_df_kept):
     """ Change columns names to  match the SQL database.
     """
-    return pandas_df_kept.rename(columns={"main.temp": "temp", "main.temp_min": "temp_min", "main.temp_max": "temp_max", "main.pressure": "pressure", "main.humidity": "humidity", "wind.speed": "wind_speed", "wind.deg": "wind_deg"})
+    return pandas_df_kept.rename(columns={'main.temp': 'temp', 'main.temp_min': 'temp_min', 'main.temp_max': 'temp_max', 'main.pressure': 'pressure', 'main.humidity': 'humidity', 'wind.speed': 'wind_speed', 'wind.deg': 'wind_deg'})
 
 
 def convert_kelvin_to_celsius(pandas_df_kept_changed):
