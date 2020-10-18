18d349b1ce496b90e2422faa00cb665a4b6d6b51
shellyalmo <33236921+shellyalmo@users.noreply.github.com>
2020-10-18 20:14:37 +0300

    add frequency as optional argument

--- src/main.py
+++ src/main.py
@@ -6,16 +6,22 @@ from get_weather_data_from_OpenWeatherMap import get_current_weather_data_from_O
 import argparse
 import time
 
-def start():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--city_id", default="293397", help="enter city id")
-    args = parser.parse_args()
-    if args.city_id:
-        print("city id inserted")
+parser = argparse.ArgumentParser()
+parser.add_argument("--city_id", default="293397", help="Fetch weather data for a specified city. Find the id of the city on OpenWeatherMap.org")
+parser.add_argument("--frequency", default=900, type=int, help="How often does the program run in seconds")
+args = parser.parse_args()
+if args.city_id:
+    print("city id inserted")
+if args.city_id:
+    print("program frequency inserted")
 
+
+def start():
     returned_connection = create_DB_connection()
-    filename = write_weather_data_in_json_file(get_current_weather_data_from_OpenWeatherMap(args.city_id))
-    new_df_to_sql = set_datetime_col_as_row_index(convert_kelvin_to_celsius(change_col_name(keep_columns(convert_dict_to_df(filename)))))
+    filename = write_weather_data_in_json_file(
+        get_current_weather_data_from_OpenWeatherMap(args.city_id))
+    new_df_to_sql = set_datetime_col_as_row_index(convert_kelvin_to_celsius(
+        change_col_name(keep_columns(convert_dict_to_df(filename)))))
 
     update_db(new_df_to_sql, returned_connection)
 
@@ -24,4 +30,4 @@ def start():
 
 while True:
     start()
-    time.sleep(900)
+    time.sleep(args.frequency)
